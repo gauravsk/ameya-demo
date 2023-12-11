be5b2a3b60e3be26e6cb768570e8fec1af917b1a
Gaurav Kandlikar <gaurav.kandlikar@gmail.com>
2023-12-11 07:50:36 -0600

    changed color of points and removed background

--- make_plot.R
+++ make_plot.R
@@ -1,9 +1,10 @@
 # Subsample fastq files
-# 11 December 2023
+# 12 December 2023
 
 library(ggplot2)
 library(tidyverse)
 tibble(a = 1:10) |> 
   ggplot(aes(x = a, y = 1)) + 
-  geom_point()
+  geom_point(size = 4, color = "darkgreen") + 
+  theme_classic()
 
